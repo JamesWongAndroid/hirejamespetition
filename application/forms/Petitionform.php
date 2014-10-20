b10467cfe1ed2b295fd3cbf941eebe6210fd38d0
JamesWongAndroid <james.wong.android@gmail.com>
2014-10-20 16:59:20 -0400

    Form update
    Added elements to form

--- application/forms/Petitionform.php
+++ application/forms/Petitionform.php
@@ -5,7 +5,41 @@ class Application_Form_Petitionform extends Zend_Form
 
     public function init()
     {
-        /* Form Elements & Other Definitions Here ... */
+        $this->setMethod('POST');
+
+        $this->addElement('text', 'first_name', array(
+        	'label' => 'First Name',));
+
+        $this->addElement('text','last_name', array(
+        	'label'		=> 'Last Name'));
+
+        $this->addElement('text','email', array(
+        	'label' 	=> 'Email Address',
+        	'required'	=> true,
+        	'filters'	=> array('StringTrim'),
+        	'validators'	=> array('EmailAddress')
+        	)); 
+
+        $this->addElement('text', 'location', array(
+        	'label'		=> 'Your Location'));
+
+        $this->addElement('textarea', 'message', array(
+        	'label'		=> 'Your Message: ',
+        	'width'		=> '150',
+        	'height'	=> '100'));
+
+        // Add the submit button
+        $this->addElement('submit', 'submit', array(
+            'ignore'   => true,
+            'label'    => 'Sign Petition',
+        ));
+
+        // CSRF protection
+        $this->addElement('hash', 'csrf', array(
+            'ignore' => true,
+        ));
+
+
     }
 
 
